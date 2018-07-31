d56ef4129af675ff923b9ada6600b9baf75657bb
Martin Keck <contact@martinkeck.com>
2018-08-01 00:03:10 +0200

    Add test form

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,6 +26,18 @@ class RootIndex extends React.Component {
             })}
           </ul>
         </div>
+
+		  <form name="contact" netlify>
+			  <p>
+				  <label>Name <input type="text" name="name" /></label>
+			  </p>
+			  <p>
+				  <label>Email <input type="email" name="email" /></label>
+			  </p>
+			  <p>
+				  <button type="submit">Send</button>
+			  </p>
+		  </form>
       </div>
     )
   }
